c5edb4cacd34b2a070679ccfab89f331b3839874
AngadSandhu <angad.sandhu16@gmail.com>
2023-05-22 20:02:22 +0530

    22May2023: Added Weather API

---
+++ src/weather.js
@@ -0,0 +1,13 @@
+const request = require("postman-request");
+const chalk = require("chalk");
+require("dotenv").config({ path: "../.env"});
+
+const currentWeatherURL = process.env.WEATHER_URL + "/current.json?key=" + process.env.WEATHER_API_KEY + encodeURIComponent("&") + "q=Chandigarh"; 
+
+request({ url: currentWeatherURL },(error,response)=> {
+    if(response){
+        console.log(chalk.green("Success"));
+        console.log(response);
+    }
+    
+});
