082537e8623bf17f8a06d4da5ba03b2c568050f1
AngadSandhu <angad.sandhu16@gmail.com>
2023-05-23 16:20:04 +0530

    23May2023: Geocode Callback Patterns

--- src/geocode.js
+++ src/geocode.js
@@ -2,20 +2,23 @@ const request = require("postman-request");
 const chalk = require("chalk");
 require("dotenv").config({ path: "../.env"});
 
-
-const geoCodeURL = process.env.GEOCODE_URL + encodeURIComponent("Toronto") + ".json"; 
-
-request({ url: geoCodeURL },(error,response)=> {
-    if(response){
-        console.log(chalk.green("Success!"),response);
-        let coordinates = {
-            latitude: response.features[0].center[1],
-            longitude: response.feature[0].center[0]
+const geoCode = (location, geoCodeCallback) => {
+    const geoCodeURL = process.env.GEOCODE_URL + encodeURIComponent(location) + ".json"; 
+    request({ url: geoCodeURL },(error,response)=> {
+        if(response){
+            console.log(chalk.green("Success!"),response);
+            let coordinates = {
+                latitude: response.features[0].center[1],
+                longitude: response.feature[0].center[0]
+            }
+            console.log("Coordinates are: ", coordinates.latitude,coordinates.longitude);
+            geoCodeCallback(undefined, coordinates);
         }
-        console.log("Coordinates are: ", coordinates.latitude,coordinates.longitude);
-    }
-    if(error){
-        console.log(chalk.red("An error occured"));
-        console.log(error);
-    }
-});
+        if(error){
+            console.log(chalk.red("An error occured"));
+            geoCodeCallback(error,undefined)
+        }
+    });
+};
+
+module.exports = geoCode;
