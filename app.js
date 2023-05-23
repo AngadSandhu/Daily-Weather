73e55ce04f4903f91f935b137fb95b6002bf0296
AngadSandhu <angad.sandhu16@gmail.com>
2023-05-23 15:58:44 +0530

    23May2023: Added Handlebars Template

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const path = require("path");
 // Initialize Express
 const app = express();
 
+const publicDirPath = path.join(__dirname,"../public");
+app.set("view engine", "hbs");                          // Sets the view engine to hbs
+app.use(express.static(publicDirPath));                 // Used for serving static content
+
 app.get("/", (req,res)=>{
+    res.render("home");
     res.send("Welocme to Daily Weather App");
 });
 
 app.listen(3000,()=>{
-    console.log("Daily Weather")
+    console.log("Daily Weather app started.. ")
 })
--- src/geocode.js
+++ src/geocode.js
@@ -7,7 +7,12 @@ const geoCodeURL = process.env.GEOCODE_URL + encodeURIComponent("Toronto") + ".j
 
 request({ url: geoCodeURL },(error,response)=> {
     if(response){
-        console.log(chalk.green("Success!"));
+        console.log(chalk.green("Success!"),response);
+        let coordinates = {
+            latitude: response.features[0].center[1],
+            longitude: response.feature[0].center[0]
+        }
+        console.log("Coordinates are: ", coordinates.latitude,coordinates.longitude);
     }
     if(error){
         console.log(chalk.red("An error occured"));
