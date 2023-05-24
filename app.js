dc08fc81d00284ffb4d506a51a98a5e94d261fa3
AngadSandhu <angad.sandhu16@gmail.com>
2023-05-24 21:00:51 +0530

    24May2023: Added Views path in express

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 const geoCode =  require("./src/geocode");
 const getWeather =  require("./src/weather");
 
 // Initialize Express
 const app = express();
 
+// Setting up paths for public and views directory
 const publicDirPath = path.join(__dirname,"../public");
+const viewsDirPath = path.join(__dirname,"../templates/views");
+
 app.set("view engine", "hbs");                          // Sets the view engine to hbs
+app.set("views", viewsDirPath)                          // Sets the path to views directory for hbs
+
 app.use(express.static(publicDirPath));                 // Used for serving static content
 
 app.get("/home", (req,res)=>{
